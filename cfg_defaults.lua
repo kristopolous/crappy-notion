26b117d939851bb6dd36ae0cf8677279a3935122
Chris McKenzie <kristopolous@yahoo.com>
2014-05-15 15:32:05 -0700

    default stuff

---
+++ cfg_defaults.lua
@@ -0,0 +1,16 @@
+--
+-- Notion default settings
+--
+
+dopath("cfg_notioncore")
+dopath("cfg_kludges")
+dopath("cfg_layouts")
+
+dopath("mod_query")
+dopath("mod_menu")
+dopath("mod_tiling")
+dopath("mod_statusbar")
+dopath("mod_sp")
+
+-- Debian extra configuration files
+dopath("cfg_debian_ext")
