fa8c95f8c4d81675f22b45fb28d205de7285d06b
Chris McKenzie <kristopolous@yahoo.com>
2019-05-08 09:17:50 -0700

    Increasing the font size for my high res display

--- look_brownsteel.lua
+++ look_brownsteel.lua
@@ -5,43 +5,43 @@ if not gr.select_engine("de") then return end
 de.reset()
 
 de.defstyle("*", {
-    shadow_colour = "#404040",
+    shadow_colour = "#202020",
     highlight_colour = "#707070",
-    background_colour = "#505050",
-    foreground_colour = "#a0a0a0",
+    background_colour = "#202020",
+    foreground_colour = "#f0f0f0",
     padding_pixels = 1,
     highlight_pixels = 1,
     shadow_pixels = 1,
     border_style = "elevated",
-    font = "-*-helvetica-medium-r-normal-*-14-*-*-*-*-*-*-*",
+    font = "-*-helvetica-medium-r-normal-*-18-*-*-*-*-*-*-*",
     text_align = "center",
 })
 
 de.defstyle("tab", {
-    font = "-*-helvetica-medium-r-normal-*-12-*-*-*-*-*-*-*",
+    font = "-*-helvetica-medium-r-normal-*-16-*-*-*-*-*-*-*",
     de.substyle("active-selected", {
         shadow_colour = "#304050",
         highlight_colour = "#708090",
-        background_colour = "#506070",
+        background_colour = "#203040",
         foreground_colour = "#ffffff",
     }),
     de.substyle("active-unselected", {
         shadow_colour = "#203040",
         highlight_colour = "#607080",
         background_colour = "#405060",
-        foreground_colour = "#a0a0a0",
+        foreground_colour = "#f0f0f0",
     }),
     de.substyle("inactive-selected", {
         shadow_colour = "#404040",
         highlight_colour = "#909090",
         background_colour = "#606060",
-        foreground_colour = "#a0a0a0",
+        foreground_colour = "#f0f0f0",
     }),
     de.substyle("inactive-unselected", {
         shadow_colour = "#404040",
         highlight_colour = "#707070",
         background_colour = "#505050",
-        foreground_colour = "#a0a0a0",
+        foreground_colour = "#f0f0f0",
     }),
     text_align = "center",
 })
