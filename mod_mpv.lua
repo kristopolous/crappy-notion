26110473d4b7cce0e43ed2fa22bd14c4928920fb
Chris McKenzie <kristopolous@yahoo.com>
2020-04-14 21:02:00 -0700

    adding a details command

--- mod_mpv.lua
+++ mod_mpv.lua
@@ -1,5 +1,5 @@
 
-keyList = { 'space', 'Right', 'Left', 'Down', 'Up', 'comma', 'period', 'q', 'r', '3', '4', '5', 'p', 'u', 'Return'}
+keyList = { 'd', 'space', 'Right', 'Left', 'Down', 'Up', 'comma', 'period', 'q', 'r', '3', '4', '5', 'p', 'u', 'Return'}
 for i, key in ipairs(keyList) do
   defbindings("WScreen", {
      kpress(MPV..key, "notioncore.exec_on(_, 'mpv-control " .. key:lower() .. "')") 
