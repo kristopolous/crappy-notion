c5c996254ff1b64aad502ed88f83ee8e6d93d2b5
Chris McKenzie <kristopolous@yahoo.com>
2020-11-15 14:37:44 -0800

    switching switch_to back

--- cfg_notion.lua
+++ cfg_notion.lua
@@ -82,7 +82,7 @@ ioncore.set{
     -- update the focus list immediately.
     --focuslist_insert_delay=1000,
 
-    switchto=false,
+    switchto=true,
     -- If enabled, activity notifiers are displayed on ALL the screens, not just
     -- the screen that contains the window producing the notification. This is
     -- only relevant on multi-head setups. By default this is disabled.
