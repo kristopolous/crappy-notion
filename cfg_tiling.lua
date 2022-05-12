c1fc46e056c2da3d706eab4ff057421ee59c0d4e
Chris McKenzie <kristopolous@yahoo.com>
2022-05-11 18:42:29 -0700

    updating bindings

--- cfg_tiling.lua
+++ cfg_tiling.lua
@@ -13,6 +13,9 @@ defbindings("WTiling", {
     kpress(META.."N", "ioncore.goto_next(_sub, 'down', {no_ascend=_})"),
     kpress(META.."1", "ioncore.goto_next(_sub, 'left')"),
 
+    kpress(META..'minus', "WTiling.split_at(_, _sub, 'bottom', true)"),
+    kpress(META..'slash', "WTiling.split_at(_, _sub, 'right', true)"),
+
     bdoc("Forward-circulate focus.", "->frame"),
     kpress(META.."Tab", "WFrame.switch_next(_)"),
 
